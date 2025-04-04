477eb4f31cab53767ed8995c869720a3a26ae3f5
youdame <112458620+youdame@users.noreply.github.com>
2025-04-05 07:04:56 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,11 +1,14 @@
 def solution(s):
     arr = s.split(" ")
     arr2 = []
-
-    for word in arr:
-        if word == "":
-            arr2.append("")  # 공백 유지
+    for i in arr:
+        문자 = i
+        if 문자 == "":
+            arr2.append(문자)
+            continue
         else:
-            arr2.append(word[0].upper() + word[1:].lower())
-
+            첫글자 = 문자[0].upper()
+            나머지 = 문자[1:].lower()
+            arr2.append(첫글자+나머지)
+        
     return " ".join(arr2)
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 0.01 ms
+메모리: 9.37 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 05:25:06
+2025년 04월 05일 07:04:52
 
 ### 문제 설명
 
