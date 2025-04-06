d61a5935fda0ab461c8372ece675441d4506eb1e
youdame <112458620+youdame@users.noreply.github.com>
2025-04-06 14:11:01 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.95 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,22 @@
+def 이진변환(num):
+    return list(bin(num)[2:])  # 문자열 리스트로 반환
+
+def 영제거함수(arr): 
+    removed_zero_list = [element for element in arr if element != "0"]
+    제거된영개수 = len(arr) - len(removed_zero_list)
+    return 제거된영개수, removed_zero_list
+
+def solution(s):
+    num_list = list(s)
+    removed_zero_num_count = 0
+    count = 0
+
+    while True:
+        이번에제거된영개수, removed_zero_list = 영제거함수(num_list)
+        removed_zero_num_count += 이번에제거된영개수
+        num_list = 이진변환(len(removed_zero_list))
+        count += 1
+        if num_list == ['1']:
+            break
+
+    return [count, removed_zero_num_count]
