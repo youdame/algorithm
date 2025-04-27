24c1491d6fc10d31f11f4da06ef49af88495c063
youdame <112458620+youdame@users.noreply.github.com>
2025-04-27 22:20:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 2583.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,13 +1,12 @@
 def 약수개수(n):
-    count = 0
-    for i in range(1, int(n**0.5) + 1):
+    count = 0 
+    for i in range(1, int(n**0.5)+1):
         if n % i == 0:
             if i * i == n:
-                count += 1  # 제곱수면 하나만 추가
-            else:
-                count += 2  # i랑 n//i 둘 다 약수
-    return count
+                count +=1
+                continue
+            count += 2
+    return count 
 
-    
 def solution(number, limit, power):
     return(sum([power if 약수개수(i) > limit else 약수개수(i) for i in range(1, number+1)] ))
