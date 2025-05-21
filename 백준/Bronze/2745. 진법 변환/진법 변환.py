d4c324e346f90577027ac884d5fb4cc577a3db44
youdame <112458620+youdame@users.noreply.github.com>
2025-05-21 16:57:15 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -2,20 +2,16 @@ import sys
 
 input = sys.stdin.readline
 
-B진법수, B = input().strip().split()
+B진법수, B = input().strip().split(" ")
 
 char_to_num = {}
-
-# 0~9
 for i in range(10):
     char_to_num[str(i)] = i
 
-# A~Z
 for i in range(10, 36):
     char_to_num[chr(i + 55)] = i
 
-result = 0
+sum = 0
 for i in range(len(B진법수)):
-    result += char_to_num[B진법수[::-1][i]] * (int(B) ** i)
-
-print(result)
+    sum += char_to_num[B진법수[::-1][i]] * (int(B) ** (i))
+print(sum)
