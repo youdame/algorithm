5b4aee3bb6264ad9f2035187c67d2d5c7cc8e016
youdame <112458620+youdame@users.noreply.github.com>
2025-05-24 16:09:09 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+a = int(input())
+b = int(input())
+
+if a > 0 and b > 0:
+    print(1)
+elif a > 0 and b < 0:
+    print(4)
+elif a < 0 and b > 0:
+    print(2)
+else:
+    print(3)
