0d332d91e1687c781f4b7f6699ce8c84fdb25b86
youdame <112458620+youdame@users.noreply.github.com>
2025-07-02 17:11:00 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.py
@@ -0,0 +1,12 @@
+import sys
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split(" "))
+
+if m == 1 or m == 2:
+    print("NEWBIE!")
+elif n >= m:
+    print("OLDBIE!")
+else:
+    print("TLE!")
