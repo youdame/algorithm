32580908db768e10c217afdecb5fa25d5b848618
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-18 14:42:06 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,10 +1,13 @@
 import sys
+from io import StringIO
 
 input = sys.stdin.readline
 
-빈도 = [0] * 31
+
+arr = [0] * 30
+
 for i in range(28):
-    빈도[int(input())] += 1
-for element in [i for i in range(len(빈도)) if 빈도[i] == 0]:
-    if element != 0:
-        print(element)
+    arr[int(input()) - 1] = 1
+
+for answer in [i + 1 for i in range(len(arr)) if arr[i] == 0]:
+    print(answer)
