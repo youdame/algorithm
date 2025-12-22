cae6b21a85dc54f3a096d13dd389b10a24416180
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-22 20:13:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 140 ms, Memory: 35668 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,6 +1,4 @@
 import sys
-from io import StringIO
-
 
 
 input = sys.stdin.readline
@@ -17,16 +15,15 @@ while count <= T:
     # print(A, B)
     A_point = 0
     B_point = 0
-    while A_point < len(A):
-        # print(A_point, B_point, A[A_point], B[B_point])
-        if B_point == len(B):
-            answer += len(B) * (len(A) - A_point)
-            break
-
-        if A[A_point] > B[B_point]:
-            B_point += 1
-        else:
+    while A_point < N:
+        if B_point == M:
+            answer += M
             A_point += 1
-            answer += B_point
+        else:
+            if A[A_point] > B[B_point]:
+                B_point += 1
+            else:
+                A_point += 1
+                answer += B_point
 
     print(answer)
