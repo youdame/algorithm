92f2579b845a8b356265a9b7ace0dfecfc10bb3e
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-27 01:57:41 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 32 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -9,15 +9,13 @@ answer = 0
 
 def 자리수합(N):
     answer = 0
-    # 마지막자리수 = N % 10
-    # print(마지막자리수)
-    # answer += 마지막자리수
-
     총자리수 = len(str(N))
-    for i in range(1, 총자리수 + 1):
-        answer += i * ((pow(10, i) - 1) - (pow(10, i - 1) - 1))
 
-    answer -= (pow(10, 총자리수) - 1 - N) * (총자리수)
+    for i in range(1, 총자리수 + 1):
+        if i == 총자리수:
+            answer += i * (N - (pow(10, i - 1) - 1))
+        else:
+            answer += i * ((pow(10, i) - 1) - (pow(10, i - 1) - 1))
 
     return answer
 
