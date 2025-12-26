f58a51d44d93460a26a6b41f3cf6c10228dd92aa
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-26 18:19:46 +0900

    [Gold IV] Title: 회사 문화 1, Time: 196 ms, Memory: 43216 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,29 +1,19 @@
 import sys
 from io import StringIO
-sys.setrecursionlimit(10**7)
+
+
 input = sys.stdin.readline
 n, m = list(map(int, input().strip().split(" ")))
 parents = list(map(int, input().strip().split(" ")))
+for index in range(len(parents)):
+    parents[index] -= 1
 
-# 0번 무시
-점수판 = [0] * (n + 1)
-# 0번 무시
-children = [[] for _ in range(n + 1)]
-for i in range(len(parents)):  # 다 도는 중
-    나 = i + 1
-    부모 = parents[i]
-    if 부모 != -1:
-        children[부모].append(나)
-
-
-def 타고들어가기(현재노드):
-    for 자식 in children[현재노드]:
-        점수판[자식] += 점수판[현재노드]
-        타고들어가기(자식)
-
-
-for j in range(m):
+score = [0] * n
+for i in range(m):
     i, w = list(map(int, input().strip().split(" ")))
-    점수판[i] += w
-타고들어가기(1)
-print(" ".join(map(str, 점수판[1:])))
+    score[i - 1] += w
+real_score = [0] * n
+
+for i in range(1, n):
+    real_score[i] += real_score[parents[i]] + score[i]
+print(*real_score)
