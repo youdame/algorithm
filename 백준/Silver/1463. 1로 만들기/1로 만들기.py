31c2f3a5bbdc92081192f4b683f310e190698069
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-28 15:34:00 +0900

    [Silver III] Title: 1로 만들기, Time: 420 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,24 @@
-n = int(input())
-dp = [0] * 1000001
-for i in range(2, n+1):
-  dp[i] = dp[i-1] + 1
-  if i % 2 == 0:
-    dp[i] = min(dp[i], dp[i // 2] + 1)
+import sys
+from io import StringIO
+input = sys.stdin.readline
 
-  if i % 3 == 0:
-    dp[i] = min(dp[i], dp[i // 3] + 1)
-print(dp[n])
+N = int(input())
 
+answer = 0
+dp = [0] * (N + 1)
+dp[1] = 0
 
 
+for i in range(2, N + 1):
+
+    if i % 2 == 0 and i % 3 == 0:
+        dp[i] = min(dp[i // 2], dp[i // 3]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i // 2], dp[i - 1]) + 1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i // 3], dp[i - 1]) + 1
+    else:
+        dp[i] = dp[i - 1] + 1
+
+
+print(dp[N])
