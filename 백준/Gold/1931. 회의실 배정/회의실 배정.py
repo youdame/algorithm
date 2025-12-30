1d296acfbf89d026a8aa34f6154e2bc47d27b4c5
조유담 <112458620+youdame@users.noreply.github.com>
2025-12-30 17:09:11 +0900

    [Gold V] Title: 회의실 배정, Time: 236 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,34 +1,25 @@
 import sys
 from io import StringIO
-from queue import PriorityQueue
 
 
 input = sys.stdin.readline
 
 T = int(input())
 
-
-queue = PriorityQueue()
-for i in range(T):
-    start, end = list(map(int, input().strip().split(" ")))
-    queue.put((start, end))
-
-end = 0
-answer = []
+meetings = [tuple(map(int, input().split())) for _ in range(T)]
 
 
-while not queue.empty():
-    start1, end1 = queue.get()
-    if start1 >= end:
-        answer.append((start1, end1))
-        end = end1
+meetings.sort(key=lambda x: (x[1], x[0]))
 
-    answer_start, answer_end = answer[-1]
-    # print(start1, end1, answer_start, answer_end)
 
-    if end1 < answer_end:
-        answer.pop()
-        answer.append((start1, end1))
-        end = end1
-print(len(answer))
-# 우선순위 큐?
+answer = 0
+answer_end = 0
+for meeting in meetings:
+    start, end = meeting
+    if start < answer_end:
+        continue
+    else:
+        answer_end = end
+        answer += 1
+
+print(answer)
