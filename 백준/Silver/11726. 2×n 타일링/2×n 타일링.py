0ee8e64f5c413157a26330162596e06ce6460ec5
조유담 <112458620+youdame@users.noreply.github.com>
2026-01-02 18:09:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,17 @@
+import sys
+from io import StringIO
+
+input = sys.stdin.readline
+T = int(input())
+
+
+dp = [0] * (1 + 1000)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+
+for i in range(4, 1001):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+
+print(dp[T] % 10007)
