912a0e736318e238cd19f5a3803afc7cec83cc27
조유담 <112458620+youdame@users.noreply.github.com>
2026-01-03 15:49:30 +0900

    [Silver V] Title: 폴리오미노, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -2,32 +2,12 @@ import sys
 from io import StringIO
 
 
-
 input = sys.stdin.readline
-board = input().strip().split(".")
-answer = []
+board = input().strip()
 
-flag = True
-for index in range(len(board)):
-    if len(board[index]) % 4 == 0:
-        answer.append("AAAA" * (len(board[index]) // 4))
-    elif len(board[index]) >= 4:
-        A숫자 = len(board[index]) // 4
-        not_divide = (len(board[index]) - (len(board[index]) // 4) * 4) % 2
-        if not_divide != 0:
-            flag = False
-            print(-1)
-            break
-        B숫자 = (len(board[index]) - (len(board[index]) // 4) * 4) // 2
-        answer.append("AAAA" * A숫자 + "BB" * B숫자)
-    elif len(board[index]) % 2 == 0:
-        answer.append("BB" * (len(board[index]) // 2))
-    else:
-        flag = False
-        print(-1)
-        break
-
-    if index != len(board) - 1:
-        answer.append(".")
-if flag:
-    print("".join(answer))
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+if "X" in board:
+    print(-1)
+else:
+    print(board)
