c2c6932b721c7c52a7a2237b321029fc7636d141
조유담 <112458620+youdame@users.noreply.github.com>
2026-01-08 14:34:28 +0900

    [Silver III] Title: 01타일, Time: 352 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,12 @@
+import sys
+from io import StringIO
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [1] * (N + 1)
+
+dp[1] = 1
+for i in range(2, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+print(dp[N])
