89a680fb3ef239e80e039cc294b1a744bf83a7ef
조유담 <112458620+youdame@users.noreply.github.com>
2026-01-17 17:20:49 +0900

    [Silver I] Title: 스타트와 링크, Time: 3040 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -3,7 +3,6 @@ from io import StringIO
 from itertools import combinations
 
 
-
 input = sys.stdin.readline
 
 T = int(input())
@@ -12,26 +11,23 @@ T = int(input())
 arr = [list(map(int, input().split())) for i in range(T)]
 n = len(arr) // 2
 
-후보들 = list(range(1, T + 1))
-seen = set()
+후보들 = list(range(0, T))
 
 min_gap = 1e9
 for b in combinations(후보들, n):
-    a = tuple(sorted([i for i in 후보들 if i not in b]))
-    if a not in seen:
-        seen.add(b)
-    else:
+    if 0 in b:
         continue
+    a = tuple(sorted([i for i in 후보들 if i not in b]))
 
     a_score = 0
     b_score = 0
     for comb in combinations(a, 2):
         i, j = comb
-        a_score += arr[i - 1][j - 1]
-        a_score += arr[j - 1][i - 1]
+        a_score += arr[i][j]
+        a_score += arr[j][i]
     for comb in combinations(b, 2):
         i, j = comb
-        b_score += arr[i - 1][j - 1]
-        b_score += arr[j - 1][i - 1]
+        b_score += arr[i][j]
+        b_score += arr[j][i]
     min_gap = min(min_gap, abs(a_score - b_score))
 print(min_gap)
