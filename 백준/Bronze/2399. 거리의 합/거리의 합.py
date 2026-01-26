e61c559d08373401298c2da93c3d01965ea6c29f
조유담 <112458620+youdame@users.noreply.github.com>
2026-01-26 18:08:02 +0900

    [Bronze II] Title: 거리의 합, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.py
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -1,22 +1,18 @@
 import sys
+from io import StringIO
+
+
 input = sys.stdin.readline
 
 N = int(input())
-x = list(map(int, input().split()))
-
-x.sort()
-
-prefix = [0] * N
-prefix[0] = x[0]
-for i in range(1, N):
-    prefix[i] = prefix[i-1] + x[i]
-
+arr = sorted(list(map(int, input().split())))
 answer = 0
-for i in range(N):
-    # 왼쪽
-    if i > 0:
-        answer += x[i] * i - prefix[i-1]
-    # 오른쪽
-    answer += (prefix[N-1] - prefix[i]) - x[i] * (N - i - 1)
 
+psum = [arr[0]]
+
+for i in range(1, N):
+    psum.append(psum[i - 1] + arr[i])
+
+for j in range(1, N):
+    answer += 2 * (arr[j] * j - psum[j - 1])
 print(answer)
