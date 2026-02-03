64b4300683d8a5208e267404315508b38840436f
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-03 15:46:43 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,26 @@
+import sys 
+from io import StringIO
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+
+path = []
+visited = [False] * N
+def dfs():
+    if len(path) == M:
+        print(*path)
+        return
+    prev = None
+    for index in range((N)):
+        if prev == arr[index]:
+            continue
+        if not path or (path and path[-1] <= arr[index]):
+            path.append(arr[index])
+            prev = arr[index]
+            dfs()
+            path.pop()
+
+        
+dfs()
