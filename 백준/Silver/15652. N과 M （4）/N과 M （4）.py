0adee7b8fc91e897129dc64d55d77e3cc8efefdd
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-03 15:18:11 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,23 +1,21 @@
-import sys
+import sys 
 from io import StringIO
+
 
 
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
 path = []
-
 def dfs():
-    if len(path) == M : 
+    if len(path) == M:
         print(*path)
         return
-    for x in range(1, N + 1):
-        if not path or path[-1] <= x:
-            path.append(x)   # 선택
+    for x in list(range(1, N+1)):
+        if not path or (path and path[-1] <= x):
+            path.append(x)
             dfs()
-            path.pop()       # 선택 취소 (되돌아감)
-        
-
+            path.pop()
+            
 dfs()
