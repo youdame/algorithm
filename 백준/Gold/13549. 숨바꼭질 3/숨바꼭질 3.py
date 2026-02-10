f3213745c498673e2996a943dedcbb0b2e272422
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-10 19:47:33 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 37992 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,30 @@
+import sys 
+from io import StringIO
+from collections import deque
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+queue = deque([])
+MAX = 100001
+distance = [1e9] * MAX
+
+queue.append(N)
+distance[N] = 0
+
+
+while queue:
+    node = queue.popleft()
+    for next in (node + 1, node - 1):
+        if 0 <= next <= 100000:
+            if distance[next] > distance[node] + 1:
+                queue.append(next)
+                distance[next] = distance[node] + 1
+    next = node * 2
+    if 0 <= next <= 100000:
+        if distance[next] > distance[node]:
+            queue.appendleft(next)
+            distance[next] = distance[node]
+                
+print(distance[K])
