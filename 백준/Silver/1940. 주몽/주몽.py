72df3deb7f4f6f6efb627514555fdd4de9360eea
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-17 00:21:18 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -1,6 +1,5 @@
 import sys 
 from io import StringIO
-
 input = sys.stdin.readline
 
 N = int(input())
@@ -11,7 +10,7 @@ left = 0
 right = N - 1
 
 answer = 0
-while left < right and right < N:
+while left < right:
     if arr[left] + arr[right] < M:
         left += 1
     elif arr[left] + arr[right] > M:
@@ -19,6 +18,7 @@ while left < right and right < N:
     else:
         answer += 1
         left += 1
+        right -= 1
 
 print(answer)
         
