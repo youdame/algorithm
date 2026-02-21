235c15238b5a874328bdb2b99c4b221bfd08a6a4
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-21 16:46:15 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, to_char(DATE_OF_BIRTH, 'yyyy-mm-dd') as DATE_OF_BIRTH
-from MEMBER_PROFILE
-where  TLNO is not null and to_char(DATE_OF_BIRTH, 'mm') = '03' and gender = 'W'
-order by MEMBER_ID asc
+SELECT MEMBER_ID,	MEMBER_NAME,	GENDER,	DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE TLNO IS NOT null and MONTH(DATE_OF_BIRTH) = 3 and GENDER = 'W' 
+ORDER BY MEMBER_ID asc
--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/README.md
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 3월에 태어난 여성 회원 목록 출력하기 - 131120 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131120?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131120?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 03월 12일 18:41:28
+2026년 02월 21일 16:46:13
 
 ### 문제 설명
 
