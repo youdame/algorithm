2d39fa7c61dab0c155e038a0df7b8dde8f0eca41
조유담 <112458620+youdame@users.noreply.github.com>
2026-02-23 00:11:23 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,4 +1,5 @@
-select animal_id, name
-from animal_ins
-where intake_condition = 'Sick'
-order by animal_id asc;
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,	NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID
