073fad29ded97c720b71b215ab1b4729305e96da
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-03 17:56:51 +0900

    [Gold V] Title: 치킨 배달, Time: 880 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,7 +2,6 @@ import sys
 from io import StringIO
 from itertools import combinations
 
-
 input = sys.stdin.readline 
 
 
@@ -19,36 +18,27 @@ def get_distance(node1, node2):
 
 
 chickens = []
+houses = []
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 2:
             chickens.append((i, j))
+        if arr[i][j] == 1:
+            houses.append((i, j))
 
-def setting_chicken(arr, comb):
-    new_arr = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] == 1:
-                new_arr[i][j] = 1
-    for i, j in comb:
-        new_arr[i][j] = 2
 
-    return new_arr
-    
 min_total_chicken_distance = 1e9
+
 for comb in combinations(chickens, M):
     total_chicken_distance = 0
-    new_arr = setting_chicken(arr, comb)
     chicken_distance = [[1e9] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if new_arr[i][j] == 2:
-                for k in range(N):
-                    for l in range(N):
-                        if new_arr[k][l] == 1:
-                            distance = get_distance((k, l), (i, j))
-                            if distance < chicken_distance[k][l]:
-                                chicken_distance[k][l] = distance
+
+    for house in houses:
+        for chicken in comb:
+            distance = get_distance(chicken, house)
+            k, l = house
+            if distance < chicken_distance[k][l]:
+                chicken_distance[k][l] = distance
     for i in range(N):
         for j in range(N):
             if chicken_distance[i][j] != 1e9:
