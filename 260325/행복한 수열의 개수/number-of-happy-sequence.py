c19ec20757b5ae94d5c4e6dc3af531ae14bb9c97
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-25 19:50:23 +0900

    Title: 행복한 수열의 개수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260325/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260325/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,27 +4,37 @@ grid = [list(map(int, input().split())) for _ in range(N)]
 어떤 숫자든 상관없고 연속해서 M개의 숫자가 나와야함 
 행으로 쭉 보고, 열로 쭉 보기
 
-(0, 0)   (0, 1)
-(n-1, 0) (n-1, 1)
+
 """
 answer = 0
 for row in grid:
-    count = 0
-    for i in range(0, N-1):
-        # 연속해서 count만큼의 숫자가 나오는지 확인하는 법,,,, 
-        # 새로운 숫자가 나오면 갱신 
+    count = 1
+    for i in range(N-1):
         if row[i] == row[i+1]:
             count += 1
         else:
-            count = 0
-        if count == M:
+            count = 1
+        if count == M :
             answer += 1 
             break
-    print(count)
-    print(answer)
+"""
+(0, 0)   (0, 1)
+(n-1, 0) (n-1, 1)
+"""
 
 for i in range(N):
     col = []
     for j in range(N):
         col.append(grid[j][i])
-    print(col)
+    # print(col)
+    count = 1
+    for k in range(N-1):
+        if col[k] == col[k+1]:
+            count += 1
+
+        else:
+            count = 1
+        if count == M :
+            answer += 1 
+            break
+print(answer)
