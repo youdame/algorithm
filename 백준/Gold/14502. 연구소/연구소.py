66eae5f250fb70a637b7351c571b5b995959d3b7
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-25 21:39:38 +0900

    [Gold IV] Title: 연구소, Time: 7148 ms, Memory: 36024 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,89 @@
+import sys 
+from io import StringIO
+from itertools import combinations
+from collections import deque
+
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = []
+for i in range(N):
+    arr.append(list( map(int, input().split())))
+
+"""
+바이러스가 유출된 상태 
+확산을 막기 위해 연구소에 벽을 세우려고 한다
+
+연구소 = 빈칸, 벽, 바이러스으로 이뤄짐 (0, 1, 2)
+
+벽을 세우지 않으면 바이러스가 상하좌우로 퍼짐
+3개의 벽을 세워서 바이러스가 최대한 퍼지지 않게 하기 
+"""
+
+empty_locations = []
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 0:
+            empty_locations.append((i, j))
+
+visited = [[False] * M for _ in range(M)]
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+def bfs(temp):
+    while queue:
+        y, x = queue.popleft() 
+        # 감염 처리 
+        temp[y][x] = 2 
+        
+        for dy, dx in directions:
+            ny, nx = dy+y, dx+x
+            if 0 <= ny < N and 0 <= nx < M:
+                if temp[ny][nx] == 0:
+                    queue.append((ny, nx))
+                    
+                    
+
+answer = 0
+
+# 조합으로 3개 뽑기 
+for comb in combinations(empty_locations, 3):
+
+    queue = deque([])
+    
+    # 원본 배열을 보존하기 위해 copy
+    temp = [row[:] for row in arr]
+    
+    # 벽 세우기 
+    for ey, ex in comb:
+        temp[ey][ex] = 1
+    
+    # 바이러스마다 bfs 시작 
+    for y in range(N):
+        for x in range(M):
+            # 바이러스는 한 번에 여러개 퍼지는 거니까.. 그냥 다 큐에 넣기
+            if temp[y][x] == 2:
+                queue.append((y, x))
+    bfs(temp)
+    
+
+    # 안전 영역 계산 
+    safe_area = 0            
+    for y in range(N):
+        for x in range(M):
+            if temp[y][x] == 0:
+                safe_area += 1
+    
+    answer = max(safe_area, answer)
+
+
+    for ey, ex in comb:
+        # 벽 되돌리기 
+        temp[ey][ex] = 0
+
+    
+print(answer)
+    
+
