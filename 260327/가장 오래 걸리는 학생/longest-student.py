2febbc9a1b81e417da75868984cda9d68a97b9eb
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-27 14:33:19 +0900

    Title: 가장 오래 걸리는 학생, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260327/가장 오래 걸리는 학생/longest-student.py
+++ 260327/가장 오래 걸리는 학생/longest-student.py
@@ -1,3 +1,4 @@
+import heapq
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
@@ -7,17 +8,41 @@ edges = [tuple(map(int, input().split())) for _ in range(m)]
 다익스트라 알고리즘 
 최단 거리 구하는 문제 
 그냥 bfs 돌면서 거리 갱신해주면 되는 거 아님?
+
+
+1에서 n번까지 가보기 ~~~~
+2에서 n번까지 
+3에서 n번까지
 """
 
-adj = {}
+adj = [[] for _ in range(n+1)]
 
-distance = [[1e9] * n for _ in range(n)]
+
 for i, j, d in edges:
-    if j not in adj or i not in adj:
-        adj[j] = [(i, d)]
-        adj[i] = [(j, d)]
-    else:
-        adj[j].append((i, d))
-        adj[i].append((j, d))
-print(adj)
+    adj[j].append((i, d))
+    adj[i].append((j, d))
+
 school = n
+
+answer = 0
+for start in range(1, n):
+    distances = [1e9] * (n + 1) 
+    distances[start] = 0
+
+    heap = []
+    heapq.heappush(heap, (0, start))
+    
+    while heap:
+        dist, node = heapq.heappop(heap)
+
+        if dist > distances[node]:
+            continue
+
+        for adj_node, adj_dist in adj[node]:
+            # 현재 node를 거쳐서 가는 게 좋은지? -> 
+            if distances[adj_node] > distances[node] + adj_dist:
+                distances[adj_node] = distances[node] + adj_dist
+                heapq.heappush(heap, (distances[adj_node], adj_node,))
+    answer = max(distances[n] , answer)
+
+print(answer)
