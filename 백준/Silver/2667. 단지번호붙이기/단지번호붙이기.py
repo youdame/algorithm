c43446d759bc357f128429559298d4af2a57a923
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-30 21:21:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 37728 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,44 +1,49 @@
 import sys
 from io import StringIO
+from collections import deque
+
 
 
 input = sys.stdin.readline
+
+
 N = int(input())
-matrix = [list(map(int, input().strip())) for i in range(N)]
-visited = [[False] * N for i in range(N)]
-# print(matrix)
-directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+arr = []
+
+for _ in range(N):
+    arr.append(list(map(int, input().strip())))
+
+visited = [[False] * N for _ in range(N)]
+
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+def bfs(node):
+    count = 1
+    i, j = node 
+    queue = deque([(i, j)])
+    visited[i][j] = True
 
 
-def dfs(node):
-    node_count = 1
-    stack = [node]
-    visited[node[0]][node[1]] = True
-    while stack:
-        y, x = stack.pop()
+    while queue:
+        y, x = queue.popleft()
         for dy, dx in directions:
-            ny = dy + y
-            nx = dx + x
-            if (
-                0 <= nx < N
-                and 0 <= ny < N
-                and not visited[ny][nx]
-                and matrix[ny][nx] == 1
-            ):
-                visited[ny][nx] = True
-                stack.append((ny, nx))
-                node_count += 1
-    return node_count
+            ny , nx = dy+ y , dx + x
 
+            if 0 <= ny < N and 0 <= nx < N:
+                if not visited[ny][nx] and arr[ny][nx] == 1:
+                    queue.append((ny, nx))
+                    visited[ny][nx] = True
+                    count += 1
+    return count 
 
-count = 0
-answer = []
-for y in range(N):
-    for x in range(N):
-        if not visited[y][x] and matrix[y][x] == 1:
-            answer.append(dfs((y, x)))
-            count = count + 1
-
-print(count)
-for element in sorted(answer):
+단지_count = 0
+마을_count_arr = []    
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j] and arr[i][j] == 1:
+            마을_count = bfs((i, j))
+            마을_count_arr.append(마을_count)
+            단지_count += 1
+            
+print(단지_count)
+for element in sorted(마을_count_arr):
     print(element)
