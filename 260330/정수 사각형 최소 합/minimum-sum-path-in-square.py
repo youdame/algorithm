9ecbc1febf07639208dc31a9560d8958137f52d5
조유담 <112458620+youdame@users.noreply.github.com>
2026-03-30 22:49:17 +0900

    Title: 정수 사각형 최소 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 260330/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -13,4 +13,13 @@ dp[i][j]란 i, j까지 가기 위한 최솟값
 
 dp = [[0] * n for _ in range(n)] 
 
+for i in range(n):
+    for j in range(n-1, -1, -1):
+        if 0 <= j+1 < n and 0 <= i-1 < n:
+            dp[i][j] = min(dp[i][j+1], dp[i-1][j]) + grid[i][j]
+        elif 0 <= j+1 < n:
+            dp[i][j] = dp[i][j+1] + grid[i][j]
+        else:
+            dp[i][j] = dp[i-1][j] + grid[i][j]
 
+print(dp[n-1][0])
