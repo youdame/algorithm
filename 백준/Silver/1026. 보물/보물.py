820fca4ebc81297c827da562e8071e9965ff2703
조유담 <112458620+youdame@users.noreply.github.com>
2026-04-01 21:15:24 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 from io import StringIO
 
 
@@ -6,12 +6,24 @@ input = sys.stdin.readline
 
 N = int(input())
 
-A_arr = sorted(list(map(int, input().split())))
-B_arr = sorted(list(map(int, input().split())), reverse = True)
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
+
+
+# S의 값을 가장 작게 만들기 위해서 수를 배ㅕㅇㄹ하자.. 가장 큰 수랑 가장 작은 수랑 곱해지면 되나?
+"""
+작은 수랑 작은 수랑 만난다? 
+
+A는 가장 큰 순ㄴ으로 나열
+B는 반대로 나열
+"""
+
+
+A.sort()
+B.sort(reverse = True)
 
 answer = 0
-for index in range(N):
-    answer += A_arr[index] * B_arr[index]
-
+for i in range(N) :
+    answer +=A[i] * B[i]
 print(answer)
