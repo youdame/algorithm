170985ed01f32a72f57317767c42e05b530a1bbe
조유담 <112458620+youdame@users.noreply.github.com>
2026-04-03 21:20:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 68 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,39 +1,50 @@
-import sys 
+import sys
+from io import StringIO
+
 
 input = sys.stdin.readline
 
-K, N = map(int, input().split()) 
-arr = []
-    
-for _ in range(K):
-    arr.append(int(input()))
+"""
+N개의 랜선을 만들어야하는데, 현재 K개만 있음 
 
-# 같은 크기의 랜선을 11개 만드려고 한다
-# 이분 탐색.. N개를 만들 수 있는 랜선의 최대 길이 = mid 
+K <= N 이겠네?
+
+K를 잘라서 N개를 만들어야함 
+N개는 모두 같은 길이임
+
+N이 가장 크게 랜선을 자르려고 할 때, N이 가장 큰 경우 
+
+딱봐도 이분탐색 .. 
+"""
+K, N = map(int, input().split())
+
+lensuns = []
+for i in range(K):
+    lensuns.append(int(input()))
+
 
 left = 1
-right = max(arr)
-answer = 0
+right = max(lensuns)
+
+
+def cal_lensun_count(lensun_length):
+    count = 0
+    for element in lensuns:
+        count += element // lensun_length
+
+    return count
+
+answer = -1e9
 while left <= right:
     mid = (left + right) // 2
 
-    count = 0
-    for element in arr:
-        count += (element // mid)
+    count = cal_lensun_count(mid)
 
+    # N개보다 더 많이 만들거나 N개를 만든 경우 => 더 가능한 지 보기 => mid는 크면 클수록 좋으므로 mid를 더 늘려보기
     if count >= N:
-        answer = mid
-        # 더 큰 mid를 찾아봐야함
+        answer = max(mid, answer)
         left = mid + 1
     else:
-        # 더 작은 mid를 찾아봐야함
+        # N개보다 적게 만든 경우 => 너무 mid가 큼 => 줄여야함 
         right = mid - 1
 print(answer)
-        
-        
-        
-        
-        
-    
-
-
