0f1805fbe86512eba8327805e88523fed2e1118e
조유담 <112458620+youdame@users.noreply.github.com>
2026-04-04 21:28:56 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3952 ms, Memory: 190888 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -5,51 +5,45 @@ from io import StringIO
 
 input = sys.stdin.readline
 
-
 arr = []
 
 N, M = map(int, input().split())
 for _ in range(N):
     arr.append(list(map(int, input().strip())))
 
+
 directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-wall_locations = []
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 1:
-            wall_locations.append((i, j))
-
-
-visited = [[[0] * 2 for _ in range(M)]  for _ in range(N)]
-distances = [[[0] * 2 for _ in range(M)] for _ in range(N)]
-
 def bfs():
-    visited[0][0][0] = True
-    distances[0][0][0] = 1
-
-    # 좌표 + 깨짐 여부 
-    queue = deque([(0, 0, 0)])
+    # visited_distance[i][j][broken] = broken 여부에 따른 i,j까지 가는 거리
     
+    visited_distance = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+    visited_distance[0][0][0] = 1
+    queue = deque([(0, 0, 0)])
+
     while queue:
         y, x, broken = queue.popleft()
-
-        # 목적지 도달 시 해당 상태의 거리를 반환
         if y == N - 1 and x == M - 1:
-            return distances[y][x][broken]
-            
+            return visited_distance[y][x][broken]
+
         for dy, dx in directions:
             ny, nx = dy + y, dx + x
 
-            if 0 <= ny < N and 0 <= nx < M :
-                if arr[ny][nx] == 0 and not visited[ny][nx][broken] :
-                    visited[ny][nx][broken] = True
-                    distances[ny][nx][broken] = distances[y][x][broken] + 1
-                    queue.append((ny,nx, broken))
-                    
-                elif arr[ny][nx] == 1 and not broken and not visited[ny][nx][1]:
-                    visited[ny][nx][1] = True
-                    distances[ny][nx][1] = distances[y][x][0] + 1
-                    queue.append((ny,nx, 1))
+            # 경로 안에 있고 
+            if 0 <= ny < N and 0 <= nx < M:
+                # 지금 벽이야 근데 아직 안 부셨어! 부실 수 있어!
+                if arr[ny][nx] == 1 and broken == 0:
+                    if visited_distance[ny][nx][broken] == 0:
+                        # 부시자
+                        visited_distance[ny][nx][1] = visited_distance[y][x][0] + 1
+                        queue.append((ny, nx, 1))
+
+                # 지금 벽이 아니야 => 현상유지
+                elif arr[ny][nx] == 0:
+                    if visited_distance[ny][nx][broken] == 0:
+                        visited_distance[ny][nx][broken] = visited_distance[y][x][broken] + 1
+                        queue.append((ny, nx, broken))
     return -1
+
+
 print(bfs())
