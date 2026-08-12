191d48b6468ffa41dde0d2d003d12837a329b140
조유담 <112458620+youdame@users.noreply.github.com>
2026-08-12 21:14:13 +0900

    [level 3] Title: 단속카메라, Time: 0.97 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,14 +1,23 @@
+"""모든 차량이 고속도로를 이용하면서 단속 카메라를 한 번은 만나도록 설치
+최대한 겹치는 부분이 많아야함
+
+와 .. 근데 3개월 지났다고 기억 안나는 거 실화?
+
+"""
 
 def solution(routes):
-    
     routes.sort(key = lambda x : x[1])
-    count = 0
-    out_camera = -30001
-    for start, end in routes:
-        if start <= out_camera <= end:
+    n = len(routes)
+    
+    current_camera = routes[0][1]
+    count = 1
+    
+    for i in range(n-1):
+        start, end = routes[i+1]
+        if start <= current_camera <= end:
             continue
         else:
-            out_camera = end
-            count +=1 
-            
+            current_camera = end
+            count += 1
     return count
+    
