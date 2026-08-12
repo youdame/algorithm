3cfc6e41cca029a0a45d2ff8396928402dec9721
조유담 <112458620+youdame@users.noreply.github.com>
2026-08-12 22:13:42 +0900

    [level 2] Title: 과제 진행하기, Time: 0.65 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,51 +1,55 @@
-def calculate_minutes(start):
-    hour, minute = map(int, start.split(":"))
-    
-    return hour * 60 + minute
-
 def solution(plans):
     
-    plans.sort(key = lambda x: x[1])
-
+    plans.sort(key = lambda x : x[1])
+    n = len(plans)
     
-    times = []
+
     for i in range(len(plans)):
         name, start, playtime = plans[i]
-        plans[i][1] = calculate_minutes(start)
-        plans[i][2] = int(plans[i][2])
-        times.append(plans[i][1])
-    # print(plans)
+        hour, minutes = map(int, start.split(":"))    
+        start = hour * 60 + minutes
+        plans[i] = [name, start, int(playtime)]
+        
     
+    """ 
+    멈춘 과제의 이름, 시간
+    멈춘 과제의 배열이 비어있는지 아닌지
+    지금 가능한지 
+    """
+    answer = []
     stack = []
     
 
-    answer = []
-    for i in range(len(plans) - 1):
-        
-        time_gap = plans[i+1][1] - plans[i][1]
-        playtime = plans[i][2]
-        
-        if time_gap < playtime:
-            stack.append((plans[i][0], playtime - time_gap))
-        else:
-            answer.append(plans[i][0])
-            spare_time = time_gap - playtime
-            
-            while stack and spare_time > 0:
-                name, remain_time = stack.pop()
-                
-                if remain_time <= spare_time:
-                    answer.append(name)
-                    spare_time = spare_time - remain_time
-                else:
-                    stack.append((name, remain_time - spare_time ))
-                    spare_time = 0 
 
+    for i in range(n-1):
+
+        name, start, playtime = plans[i]
+        next_start = plans[i+1][1]
+        
+        finish = start + playtime
+        
+
+        if finish > next_start:
+            stack.append([name, finish - next_start])
+        else:
+            answer.append(name)
+            gap = next_start - finish
+            while stack and gap > 0:
+                remain_name, remain_playtime = stack[-1]
+                if remain_playtime <= gap: 
+                    stack.pop()
+                    answer.append(remain_name)
+                    gap -= remain_playtime
+                else:
+                    stack[-1][-1] -= gap
+                    gap = 0
+                    
     answer.append(plans[-1][0])
-       
     while stack:
-        name, remain_time = stack.pop()            
-        answer.append(name)
+        answer.append(stack.pop()[0])
+            
+                        
         
     return answer
+    
         
