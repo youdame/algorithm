f562d0b81497b75f465f353b89989f4e5e45691c
조유담 <112458620+youdame@users.noreply.github.com>
2026-09-04 16:50:03 +0900

    [level 3] Title: 등굣길, Time: 3.64 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,24 +1,21 @@
 def solution(m, n, puddles):
-    
     dp = [[0] * m for _ in range(n)]
-    dp[0][0] = 1
-    
-    
+    puddles = [[y-1, x-1] for x, y in puddles]    
     
 
+    dp[0][0] = 1
     for i in range(n):
-        for j in range(m): 
-            if i == 0 and j == 0: continue
-            if [j + 1, i + 1] in puddles: 
-                dp[i][j] = 0
+        for j in range(m):
+            if (i== 0 and j == 0) or [i, j] in puddles:
                 continue
             
             if i == 0:
                 dp[i][j] = dp[i][j-1]
-            elif j == 0 : 
+            elif j == 0:
                 dp[i][j] = dp[i-1][j]
-            else :       
-                dp[i][j] = dp[i - 1][j] + dp[i][j-1]
-                    
-    # print(dp)
-    return dp[n-1][m-1] % 1000000007
+            else:
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000007
+            
+    # print(puddles)
+    print(dp)
+    return dp[n-1][m-1]
