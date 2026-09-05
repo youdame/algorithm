0682fcb7f20ed4a419d25d019608dc292bee704e
조유담 <112458620+youdame@users.noreply.github.com>
2026-09-05 11:59:24 +0900

    [Passed] N개 중에 M개 뽑기 (56ms, 16MB)

--- trail4/N개 중에 M개 뽑기/n-choose-m.py
+++ trail4/N개 중에 M개 뽑기/n-choose-m.py
@@ -11,11 +11,11 @@ def backtrack(result, start):
         print(" ".join(map(str, result)))
         return 
 
-    for i in range(1, N + 1):
-        if i not in result and start < i:
-            result.append(i)
-            backtrack(result, i)
-            result.pop()
+    for i in range(start + 1, N + 1):
+
+        result.append(i)
+        backtrack(result, i)
+        result.pop()
 
 
 
