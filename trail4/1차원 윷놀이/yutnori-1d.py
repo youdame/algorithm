0a0dac9b775b6f47d7209bf2e69a008c55f67ebc
조유담 <112458620+youdame@users.noreply.github.com>
2026-09-07 16:33:15 +0900

    [Passed] 1차원 윷놀이 (2864ms, 25MB)

--- trail4/1차원 윷놀이/yutnori-1d.py
+++ trail4/1차원 윷놀이/yutnori-1d.py
@@ -1,38 +1,27 @@
 n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
 
-# Please write your code here.
-
-
-result = []
-
-from collections import defaultdict
-
-
-
-
 answer = -1e9
-def backtrack():
-    group_sums = [1] * (k + 1)
-    global result, answer
-    if len(result) == n:
-        # print(result)
-        for j in range(n):
-            group_sums[result[j]] += nums[j]
+group_sums = [1] * (k + 1)
+def backtrack(depth):
+    
+    global answer
+
+    if depth == n:
         pass_node = 0
-        for p in range(k+1):
+        for p in range(1, k+1):
             group_sum = group_sums[p]
             if group_sum >= m:
                 pass_node += 1
-        # print(result, record, pass_node)
         answer = max(answer, pass_node)
                 
         return 
     for i in range(1, k+1):
-        result.append(i)
-        backtrack()
-        result.pop()
+        group_sums[i] += nums[depth]
+        
+        backtrack(depth + 1)
+        group_sums[i] -= nums[depth]
 
 
-backtrack()
+backtrack(0)
 print(answer)
