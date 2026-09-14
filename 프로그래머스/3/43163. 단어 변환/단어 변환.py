eedd2b86409a4b0e090b3eede77e72b0207e4a7a
조유담 <112458620+youdame@users.noreply.github.com>
2026-09-14 18:52:36 +0900

    [level 3] Title: 단어 변환, Time: 0.80 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,26 +1,61 @@
-from collections import deque
+"""
+
+
+backtracking 아닌가..?
+한 알파벳만 다르면 바꿀 수 있음 -> 그걸 어케 알ㄴ지..
+
+하나의 알파벳만 다르다 
+-> 나머지는 다 같고 하나만 다르다
+-> 이걸 어떻게 빨리 알지..?
+
+
+"""
+from collections import defaultdict
 
 def solution(begin, target, words):
-    n = len(words)
-    visited = [False] * n
-    queue = deque([(begin, 0)])
+
+    if target not in words:
+        return 0
     
-    while queue:
-        current_node, current_dist = queue.popleft()
-        
-        if current_node == target:
-            return current_dist
-        
-       
-        for word in words:
-            miss_count = 0
-            for i in range(len(word)):
-                if word[i] != current_node[i]:
-                    miss_count += 1
-            word_index = words.index(word)
-            
-            if miss_count == 1 and not visited[word_index]:
-                queue.append((word, current_dist + 1))
-                visited[word_index] = True
+    words.append(begin)
+    adj = defaultdict(set)
+    word_len = len(words[0])
+    words_len = len(words)
+    for i in range(words_len):
+        i_word = words[i]
+        for j in range(words_len):
+            if i != j:
+                j_word = words[j]
                 
-    return 0
+                count = 0
+                for k in range(word_len):
+                    if i_word[k] == j_word[k]:
+                        count += 1
+                if count == word_len-1:
+                    adj[i_word].add(j_word)
+                    adj[j_word].add(i_word)
+                   
+        
+    
+    answer = float("inf")
+    visited = {begin}
+    def backtrack(current_word, step):
+        nonlocal answer
+        if current_word == target:
+            answer = min(step, answer)
+            return 
+
+        for next_word in adj[current_word]:
+            if next_word not in visited:
+                visited.add(next_word)
+                backtrack(next_word, step + 1)
+                visited.remove(next_word)
+        
+        
+    backtrack(begin, 0)   
+    return answer
+        
+            
+        
+        
+    
